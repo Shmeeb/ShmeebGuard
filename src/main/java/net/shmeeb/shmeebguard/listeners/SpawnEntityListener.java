3cf8bd16dae23a1479cc4b5c4d2b4f16f8aa6807
Justin <simplycrafted2@gmail.com>
2021-05-20 22:44:08 -0700

    add ALLOW_NATURAL_SPAWNS flag

--- src/main/java/net/shmeeb/shmeebguard/listeners/SpawnEntityListener.java
+++ src/main/java/net/shmeeb/shmeebguard/listeners/SpawnEntityListener.java
@@ -1,13 +1,12 @@
 package net.shmeeb.shmeebguard.listeners;
 
+import com.pixelmonmod.pixelmon.blocks.spawning.TileEntityPixelmonSpawner;
 import com.pixelmonmod.pixelmon.entities.pixelmon.EntityPixelmon;
 import net.shmeeb.shmeebguard.ShmeebGuard;
 import net.shmeeb.shmeebguard.objects.FlagTypes;
 import net.shmeeb.shmeebguard.objects.Region;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.Item;
-import org.spongepowered.api.entity.living.monster.Creeper;
-import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.EventListener;
 import org.spongepowered.api.event.entity.SpawnEntityEvent;
 
@@ -20,17 +19,25 @@ public class SpawnEntityListener implements EventListener<SpawnEntityEvent> {
     public void handle(SpawnEntityEvent event) {
         if (event.isCancelled()) return;
         if (event.getEntities().isEmpty()) return;
-        Entity entity = event.getEntities().get(0);
 
+        Entity entity = event.getEntities().get(0);
         Optional<List<Region>> regions = ShmeebGuard.getRegionManager().getAllRegionsAtPosition(entity.getLocation());
         if (!regions.isPresent()) return;
 
         for (Region region : regions.get()) {
 
             if (entity instanceof EntityPixelmon) {
-                if (!region.getFlagTypes().contains(FlagTypes.SPAWN_POKEMON)) continue;
-                event.setCancelled(true);
-                return;
+
+                if (region.getFlagTypes().contains(FlagTypes.SPAWN_POKEMON)) {
+                    event.setCancelled(true);
+                    return;
+                }
+
+                if (region.getFlagTypes().contains(FlagTypes.ALLOW_NATURAL_SPAWNS) && !event.getCause().containsType(TileEntityPixelmonSpawner.class)) {
+                    event.setCancelled(true);
+                    return;
+                }
+
             } else if (entity instanceof Item) {
                 if (!region.getFlagTypes().contains(FlagTypes.DROP_ITEMS)) continue;
                 event.setCancelled(true);
