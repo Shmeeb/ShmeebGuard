8c3c04b25dc4ec9124a52c82c7bdb8bf2425e58f
Justin <simplycrafted2@gmail.com>
2022-04-13 18:58:06 -0700

    implement autobutcher flag

--- src/main/java/net/shmeeb/shmeebguard/ShmeebGuard.java
+++ src/main/java/net/shmeeb/shmeebguard/ShmeebGuard.java
@@ -5,6 +5,9 @@ import com.pixelmonmod.pixelmon.Pixelmon;
 import net.shmeeb.shmeebguard.commands.Base;
 import net.shmeeb.shmeebguard.listeners.*;
 import net.shmeeb.shmeebguard.managers.RegionManager;
+import net.shmeeb.shmeebguard.objects.FlagTypes;
+import net.shmeeb.shmeebguard.objects.Region;
+import net.shmeeb.shmeebguard.utils.Utils;
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
@@ -14,6 +17,10 @@ import org.slf4j.LoggerFactory;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.living.animal.Animal;
+import org.spongepowered.api.entity.living.monster.Monster;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.*;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
@@ -23,11 +30,14 @@ import org.spongepowered.api.event.entity.SpawnEntityEvent;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.item.inventory.DropItemEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.world.World;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
 
 @Plugin(id = "shmeebguard",
         name = "ShmeebGuard",
@@ -73,6 +83,33 @@ public class ShmeebGuard {
         regionManager = new RegionManager();
         registerEventListeners();
         registerCommands();
+
+        Task.builder().delay(10, TimeUnit.MINUTES).execute(task -> {
+            int killed = 0;
+
+            for (World world : Sponge.getServer().getWorlds()) {
+                for (Entity entity : world.getEntities()) {
+
+                    if (entity instanceof Animal || entity instanceof Monster) {
+
+                        for (Region region : ShmeebGuard.getRegionManager().getAllRegionsAtPosition(entity.getLocation())) {
+                            if (region.getFlagTypes().contains(FlagTypes.AUTOBUTCHER)) {
+                                entity.remove();
+                                killed++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (Player onlinePlayer : Sponge.getServer().getOnlinePlayers()) {
+                if (onlinePlayer.hasPermission("shmeebguard.notify")) {
+                    onlinePlayer.sendMessage(Utils.getText("&7&oShmeebGuard AutoButcher cleared " + killed + " entities"));
+                }
+            }
+
+
+        }).submit(instance);
     }
 
     public void loadConfig() {
